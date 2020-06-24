dad8e2fec167593d33b69c75ddc7637a700641e6
Daniel Åslund <daniel.aaslund@gmail.com>
2020-06-24 18:01:04 +0200

    Added industry controller

---
+++ StockAPI/Controllers/IndustryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockAPI.Models;
+
+namespace StockAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class IndustryController : ControllerBase
+    {
+        public readonly StockAppDbContext _context;
+
+        public IndustryController(StockAppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Industry>> Get()
+        {
+            return await _context.Industry.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<Industry> Get(int id)
+        {
+            return await _context.Industry.FirstOrDefaultAsync(i => i.IndustryId == id);
+        }
+    }
+}
